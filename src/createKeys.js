13de65c9855216fef46319b3ec6b65fccdd09996
SnXnJr <sanxyanjr@gmail.com>
2022-05-06 17:41:07 +0300

    feat: creat backspace, arrows, capslock

--- src/createKeys.js
+++ src/createKeys.js
@@ -1,14 +1,9 @@
+/* eslint-disable no-plusplus */
 import keys from './keys.js'
 
 export default class Keys {
-  constructor(lang = 'ru', caps = false, shift = false) {
-    this.lang = lang
-    this.caps = caps
-    this.shift = shift
-  }
-
   // eslint-disable-next-line class-methods-use-this
-  createKeys() {
+  static createKeys() {
     const keyWrapper = []
 
     Object.entries(keys).forEach((key) => {
@@ -44,10 +39,39 @@ export default class Keys {
         })
       })
 
-      //   enLang.childNodes.forEach((el) => el.classList.add('hidden'))
       keyBtn.append(ruLang, enLang)
       keyWrapper.push(keyBtn)
     })
     return keyWrapper
+  }
+
+  static init(keysArr) {
+    const app = document.createElement('div')
+    const rowWrapper = document.createElement('div')
+    const textArea = document.createElement('textarea')
+
+    const row = []
+
+    for (let i = 0; i < 5; i++) {
+      row[i] = document.createElement('div')
+      row[i].classList.add('row')
+    }
+
+    for (let i = 0; i < keysArr.length; i++) {
+      if (i < 14) row[0].append(keysArr[i])
+      if (i >= 14 && i < 29) row[1].append(keysArr[i])
+      if (i >= 29 && i < 42) row[2].append(keysArr[i])
+      if (i >= 42 && i < 55) row[3].append(keysArr[i])
+      if (i >= 55) row[4].append(keysArr[i])
+    }
+
+    app.classList.add('app')
+    rowWrapper.classList.add('wrapper')
+    textArea.classList.add('textarea')
+
+    app.append(textArea)
+    app.append(rowWrapper)
+    rowWrapper.append(...row)
+    document.body.prepend(app)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-plusplus */
 import createKeys from './createKeys.js'
 
 // eslint-disable-next-line new-cap
-const keyClass = new createKeys()
-const keysArr = keyClass.createKeys()
+const keysArr = createKeys.createKeys()
 const pressed = new Set()
+const exclude = [
+  'CapsLock',
+  'ShiftLeft',
+  'ShiftRight',
+  'ControlLeft',
+  'ControlRight',
+  'MetaLeft',
+  'AltLeft',
+  'AltRight',
+  'Tab',
+  'Delete',
+  'Enter',
+]
+
+if (!localStorage.getItem('lang')) {
+  localStorage.setItem('lang', 'ru')
+}
+
+const state = {
+  text: '',
+}
+
+function writeText(symbol) {
+  const textArea = document.querySelector('textarea')
+  if (symbol === 'Backspace') {
+    state.text = state.text.substring(0, state.text.length - 1)
+  } else {
+    state.text += symbol
+  }
+
+  textArea.value = state.text
+}
 
 function keystroke(e) {
   e.preventDefault()
   const keyCode = e.code
-  console.log(keyCode)
+  let symbol = e.key
   if (e.type === 'keydown') {
     document.querySelector(`.${keyCode}`).classList.add('active')
     pressed.add(e.code)
@@ -22,46 +54,28 @@ function keystroke(e) {
         ? localStorage.setItem('lang', 'en')
         : localStorage.setItem('lang', 'ru')
     }
+    if (keyCode === 'CapsLock') {
+      document.querySelector(`.${keyCode}`).classList.toggle('active')
+      document.querySelectorAll('.lang').forEach((items) => {
+        items.querySelectorAll('span').forEach((i) => {
+          if (i.classList.contains('caps')) i.classList.toggle('hidden')
+          if (i.classList.contains('caseDown')) i.classList.toggle('hidden')
+        })
+      })
+    }
+    if (!exclude.includes(`${keyCode}`)) {
+      keyCode === 'ArrowUp' ? (symbol = '▲') : ''
+      keyCode === 'ArrowRight' ? (symbol = '►') : ''
+      keyCode === 'ArrowDown' ? (symbol = '▼') : ''
+      keyCode === 'ArrowLeft' ? (symbol = '◄') : ''
+      writeText(symbol)
+    }
   } else if (e.type === 'keyup') {
     document.querySelector(`.${keyCode}`).classList.remove('active')
     pressed.clear()
   }
 }
 
-function init() {
-  if (!localStorage.getItem('lang')) {
-    localStorage.setItem('lang', 'ru')
-  }
-
-  const app = document.createElement('div')
-  const rowWrapper = document.createElement('div')
-  const textArea = document.createElement('textarea')
-
-  const row = []
-
-  for (let i = 0; i < 5; i++) {
-    row[i] = document.createElement('div')
-    row[i].classList.add('row')
-  }
-
-  for (let i = 0; i < keysArr.length; i++) {
-    if (i < 14) row[0].append(keysArr[i])
-    if (i >= 14 && i < 29) row[1].append(keysArr[i])
-    if (i >= 29 && i < 42) row[2].append(keysArr[i])
-    if (i >= 42 && i < 55) row[3].append(keysArr[i])
-    if (i >= 55) row[4].append(keysArr[i])
-  }
-
-  app.classList.add('app')
-  rowWrapper.classList.add('wrapper')
-  textArea.classList.add('textarea')
-
-  app.append(textArea)
-  app.append(rowWrapper)
-  rowWrapper.append(...row)
-  document.body.prepend(app)
-}
-
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener('DOMContentLoaded', createKeys.init(keysArr))
 window.addEventListener('keydown', keystroke)
 window.addEventListener('keyup', keystroke)
